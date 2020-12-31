62fc197b8e07f0047b1561521be0d849443e5f34
Ramaa <sumathi.gurusammy@gmail.com>
2020-12-30 20:27:23 -0600

    main entry after 2 days

---
+++ JavaTest/src/SystemClass.java
@@ -0,0 +1,14 @@
+
+public class SystemClass {
+	
+	public static void main(String[] args){
+		showMessage();
+	}
+	
+	public static void showMessage(){
+		System.out.println("WOnderful New Year 2021 with out deadly corona artifacts");
+		
+	}
+
+
+}
